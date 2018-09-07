80f2a177525bf16a516f63996a38b242b27c1fa7
LewK2 <lewis.kirkaldie@gmail.com>
2018-09-07 13:26:13 +0100

    -added example to access MCRItem from CAS for a node

---
+++ Cas/CAS-Query-GetMcrItemFromNode.ps1
@@ -0,0 +1,19 @@
+﻿Param(
+    [Parameter(Mandatory=$true)][string]$SourceNodeId
+)
+
+. .\CAS-Core.ps1
+
+$context = Get-CasContext
+#Template: "/export/{plugin_id}?params={export_params}". 
+$result = Invoke-CasMethod -MethodRelativeUrl "export/CinegyInventoryExport?params=$SourceNodeId" -Context $context
+
+if($result.retCode -ne 0)
+{
+    Write-Host "Error getting MCR XML for object: $($result.error)"
+}
+else {
+    Write-Host "Selected node MCR XML: $($result.resultXML)"
+}
+
+Invoke-CasLogout -Context $context
