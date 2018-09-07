afd49c0023add69fdfa6f35d12e876a8f65668d3
LewK2 <lewis.kirkaldie@gmail.com>
2018-09-07 09:58:10 +0100

    -updated to show created sequence ID in console after success in copying

--- Cas/CAS-CopySequenceToFolder.ps1
+++ Cas/CAS-CopySequenceToFolder.ps1
@@ -1,13 +1,13 @@
 Param(
     [Parameter(Mandatory=$true)][string]$SourceSequenceId,
-    [Parameter(Mandatory=$true)][string]$TargetFolder
+    [Parameter(Mandatory=$true)][string]$TargetFolderId
 )
 
 . .\CAS-Core.ps1
 
 $context = Get-CasContext
 
-$targetChildren = Invoke-CasMethod -MethodRelativeUrl "/nodes/$($TargetFolder)?f=0" -Context $context
+$targetChildren = Invoke-CasMethod -MethodRelativeUrl "/nodes/$($TargetFolderId)?f=0" -Context $context
 
 #when copying somewhere, it is required to indicate where the resulting object should be inserted among the existing children of the target location
 if($targetChildren.nodes.Length -gt 0)
@@ -18,14 +18,17 @@ if($targetChildren.nodes.Length -gt 0)
 else
 {
     #if there are no children, specify the ID of the containing folder (strange, but true)
-    $afterId = $TargetFolder    
+    $afterId = $TargetFolderId    
 }
 
-$moveResult = Invoke-CasMethod -MethodRelativeUrl "/copy?node=$SourceSequenceId&parent=$TargetFolder&position=$afterId" -Context $context -Method POST 
+$moveResult = Invoke-CasMethod -MethodRelativeUrl "/copy?node=$SourceSequenceId&parent=$TargetFolderId&position=$afterId" -Context $context -Method POST 
 
 if($moveResult.retCode -ne 0)
 {
     Write-Host "Error copying object: $($moveResult.error)"
+}
+else {
+    Write-Host "Created new sequence from source with ID: $($moveResult.node._id._nodeid_id)"
 }
 
 Invoke-CasLogout($context)
