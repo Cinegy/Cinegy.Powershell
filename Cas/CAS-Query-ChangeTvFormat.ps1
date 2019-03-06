44f21afa042adc4e8fa9b5ad659f9eb02e7d406d
Kelembet Alexandr <kelembet@cinegy.com>
2019-03-06 16:28:38 +0200

    Additional sample on getting node properties

---
+++ Cas/CAS-Query-ChangeTvFormat.ps1
@@ -0,0 +1,34 @@
+﻿. .\CAS-Core.ps1
+
+#predefined TV format field
+$fieldNum = 11
+
+$nodeId = Read-Host -Prompt "Enter node ID"
+$tvFormat = Read-Host -Prompt "Enter node new TV Format"
+
+$context = Get-CasContext
+$response = Invoke-CasMethod -MethodRelativeUrl "/node/$($nodeId)?f=3" -Context $context
+
+if($response.retCode -ne 0)
+{
+    Write-Host "Failed to locate node: $($response.error)"
+}
+else 
+{
+    Write-Host "Found node with ID: $($response.node.node._id._nodeid_id)"
+    
+    Write-host "Old TV Format: $($response.node.node.tvFormat)"
+
+    $response = Invoke-CasMethod -MethodRelativeUrl "/setproperty?node=$($nodeId)&fieldnum=$($fieldNum)&value=$($tvFormat)" -Context $context -Method POST
+
+    if($response.retCode -ne 0)
+    {
+        Write-Host "Failed to update node: $($response.error)"
+    }
+    else 
+    {
+        Write-Host "Node TV format is updated."
+    }
+}
+
+Invoke-CasLogout($context)
---
+++ Cas/CAS-Query-GetNodeProperties.ps1
@@ -0,0 +1,26 @@
+﻿. .\CAS-Core.ps1
+
+$nodeId = Read-Host -Prompt "Enter node ID"
+
+$context = Get-CasContext
+$response = Invoke-CasMethod -MethodRelativeUrl "/node/$($nodeId)?f=3" -Context $context
+
+if($response.retCode -ne 0)
+{
+    Write-Host "Failed to locate node: $($response.error)"
+}
+else 
+{
+    Write-Host "Found node with ID: $($response.node.node._id._nodeid_id)"
+    
+    Write-Host "  Tags:"
+    foreach($tag in $response.node.tags)
+    {
+        Write-Host "    $($tag)"
+    }
+
+    Write-Host "  Node:"
+    Write-Host ($response.node.node | Format-List | Out-String)
+}
+
+Invoke-CasLogout($context)
