19345627c115893b30b3d8b4a1559e6e1519040f
LewK2 <lewis.kirkaldie@gmail.com>
2019-06-12 17:06:46 +0100

    -add placeholder for CAS-Create-SequenceMarker script (WIP)

---
+++ Cas/CAS-Create-SequenceMarker.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [string]$SequenceId
+)
+
+. .\CAS-Core.ps1
+
+if(!$SequenceId)
+{
+    $SequenceId = Read-Host -Prompt "Enter sequence ID"
+}
+
+$context = Get-CasContext
+$response = Invoke-CasMethod -MethodRelativeUrl "/node/$($SequenceId)?f=3" -Context $context
+
+if($response.retCode -ne 0)
+{
+    Write-Host "Failed to locate node: $($response.error)"
+    Exit
+}
+else 
+{
+    Write-Host "Found node with ID: $($response.node.node._id._nodeid_id)"
+    
+    Write-Host "  Tags:"
+    foreach($tag in $response.node.tags)
+    {
+        Write-Host "    $($tag)"
+    }
+
+    Write-Host "  Node:"
+    Write-Host ($response.node.node | Format-List | Out-String)
+}
+
+#TODO: Add call to as-yet-not-existing CreateMarker function on CAS REST
+
+Invoke-CasLogout($context)
