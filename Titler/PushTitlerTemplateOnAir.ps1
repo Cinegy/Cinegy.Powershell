201fd9eaa367e1345faa2720edd23decb67f422e
ioCode <137887501+ioCodeAbsoyak@users.noreply.github.com>
2023-10-30 12:18:24 +0000

    + Variable Set

--- Titler/PushTitlerTemplateOnAir.ps1
+++ Titler/PushTitlerTemplateOnAir.ps1
@@ -1,4 +1,17 @@
-﻿#make a Type PostRequest XML document using .Net XML document object
+﻿#some variable values to use within the script
+$airServerAddress = "127.0.0.1"
+$airChannelNumber = 0
+$airLayerNumber = 5 #Layer Number from 0 - 8
+$templatePath = "C:\Data\Dev\Git\Cinegy.Powershell\Titler\TitlerScenes\Lower3rd.cintitle"; #Use the sample scene on TitlerScenes Folder - Lower3rd.cintitle
+$titleText = "Top Line For Title"
+$newsText = "Details of the news story for the audience to understand the plot with 3 or less sentences."
+
+$varialbleList = "<Variables>"
+$varialbleList += "<Var Name=""TopText.Text"" Type=""String"" Value=""$titleText"" />"
+$varialbleList += "<Var Name=""BottomText.Text"" Type=""String"" Value=""$newsText"" />"
+$varialbleList += "</Variables>"
+
+#make a Type PostRequest XML document using .Net XML document object
 $xmlDoc = New-Object System.Xml.XmlDocument;
 $decl = $xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", $null)
 
@@ -10,7 +23,7 @@ $xmlDoc.InsertBefore($decl, $xmlDoc.DocumentElement)
 $xmlSetValueElem = $xmlRootElem.AppendChild($xmlDoc.CreateElement('Event'));
 
 $xmlDevAttr = $xmlDoc.CreateAttribute('Device');
-$xmlDevAttr.Value = '*GFX_LOGO'; 
+$xmlDevAttr.Value = '*GFX_' + $airLayerNumber; 
 $xmlSetValueElem.Attributes.Append($xmlDevAttr);  
 
 $xmlCmdAttr = $xmlDoc.CreateAttribute('Cmd');
@@ -18,11 +31,11 @@ $xmlCmdAttr.Value = 'SHOW';
 $xmlSetValueElem.Attributes.Append($xmlCmdAttr);  
 
 $xmlSetOp1Elem = $xmlDoc.CreateElement('Op1');
-$xmlSetOp1Elem.InnerText = "D:\Data\OneDrive\Type Scenes\TestingScene.cintitle";
+$xmlSetOp1Elem.InnerText = $templatePath
 $xmlSetValueElem.AppendChild($xmlSetOp1Elem);
 
 $xmlSetOp2Elem = $xmlDoc.CreateElement('Op2');
-$xmlSetOp2Elem.InnerText = ""
+$xmlSetOp2Elem.InnerText = $varialbleList
 $xmlSetValueElem.AppendChild($xmlSetOp2Elem);
 
 $xmlSetOp3Elem = $xmlDoc.CreateElement('Op3');
@@ -37,6 +50,6 @@ $web = new-object net.webclient
 $web.Headers.add("Content-Type", "text/xml; charset=utf-8")
 
 #perform the actual HTTP post to the IP and port (which is 5521 + instance number) of the XML data
-$web.UploadString("http://localhost:5521/video/command", $xmlDoc.OuterXml)
+$web.UploadString("http://$($airServerAddress):$(5521+$airChannelNumber)/video/command", $xmlDoc.OuterXml)
 
-$xmlDoc.OuterXml | Out-File "D:\Temp\xmldoc.xml"
+$xmlDoc.OuterXml | Out-File ".\xmldoc.xml"
